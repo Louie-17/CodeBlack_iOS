b2320abfacc32f52d44c903602ba001a88a81063
taegyun0416-prog <taegyun0416@gmail.com>
2026-07-13 13:51:29 +0900

    setting

---
+++ CodeBlack/CodeBlack/CodeBlackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBlackApp.swift
+//  CodeBlack
+//
+//  Created by 장태균 on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBlackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBlack/CodeBlack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBlack
+//
+//  Created by 장태균 on 7/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
